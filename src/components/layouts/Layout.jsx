285e9d88bc5d50b37b4b65cc11af2910885a2a9b
Babucarr-Badjie <93027934+Babucarr-Badjie@users.noreply.github.com>
2025-06-17 00:21:31 +0800

    welcome page

--- src/components/layouts/Layout.jsx
+++ src/components/layouts/Layout.jsx
@@ -1,4 +1,37 @@
 export default function Layout(props) {
   const { children } = props;
-  return <div>{children}</div>;
+
+  const header = (
+    <header className="header">
+      <h1 className="text-gradient">WordJourney</h1>
+      <p>
+        <i>
+          "Embark on your journey to master new words every day. WordJourney
+          helps you learn, track, and retain vocabulary through engaging and
+          personalized learning tools."
+        </i>
+      </p>
+    </header>
+  );
+
+  const footer = (
+    <footer>
+      <p>Design & written by </p>
+      <a href="https://babucarr-badjie-portfolio.netlify.app/" target="_blank">
+        <img
+          src="https://avatars.githubusercontent.com/u/93027934?v=4"
+          alt=""
+        />
+        <p>Babucarr Badjie</p>
+        <i class="fa-regular fa-address-card"></i>
+      </a>
+    </footer>
+  );
+  return (
+    <>
+      {header}
+      <main>{children}</main>
+      {footer}
+    </>
+  );
 }
--- src/components/layouts/Welcome.jsx
+++ src/components/layouts/Welcome.jsx
@@ -1,16 +1,10 @@
 export default function Welcome() {
   return (
-    <div>
-      <h1>Welcome to Our Application!</h1>
-      <p>
-        We are excited to have you here. This application is designed to help
-        you achieve your goals with ease and efficiency.
-      </p>
-      <p>
-        To get started, please navigate through the menu to explore the features
-        we offer. If you have any questions, feel free to reach out to our
-        support team.
-      </p>
-    </div>
+    <section className="welcome">
+      <h3 className="text-large special-shadow">
+        365 Days <br />
+        365 words
+      </h3>
+    </section>
   );
 }
