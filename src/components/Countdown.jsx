223838b9041fe9f8cb17f50e56091ec8e6354b56
Babucarr-Badjie <93027934+Babucarr-Badjie@users.noreply.github.com>
2025-06-22 23:32:48 +0800

    updated countdown component

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,3 +1,7 @@
 export default function Countdown() {
-  return <div></div>;
+  return (
+    <div className="card countdown-card">
+      <h1 className="item-header">Day {1}</h1>
+    </div>
+  );
 }
