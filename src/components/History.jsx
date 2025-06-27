1983e067a0b5c69c3b023121f01efafc3e0cad7e
Babucarr-Badjie <93027934+Babucarr-Badjie@users.noreply.github.com>
2025-06-27 22:43:22 +0800

    updated

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -3,8 +3,20 @@ export default function History() {
     <div className="card history-card">
       <h4>History</h4>
       <p>
-        You have no attempts! Press <b>Start</b> to begin
+        You have no attempts! Press <b>Start</b> to begin ⭐
       </p>
+      <div className="history-list">
+        <div className="card-button-secondary">
+          <div>
+            <p>Started</p>
+            <h6>March 25 2025</h6>
+          </div>
+          <div>
+            <p>Streak</p>
+            <h6>53</h6>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
